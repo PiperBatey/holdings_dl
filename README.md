f72eb5ad44ebcffa38338663e2909c6317f2b504
pbatey <pbatey@umich.edu>
2021-07-13 21:39:02 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,147 @@
+# ETF Holdings DL
+
+A simple Python script that downloads the top 25 holdings of one or more ETFs into a .csv file.
+
+## Dependencies
+
+* [Python 3](https://www.python.org/) (tested on 3.6 and 3.9)
+* [Pandas](https://pandas.pydata.org/)
+  * [NumPy](https://numpy.org/)
+  * [lxml](https://pypi.org/project/lxml/)
+* [yfinance](https://pypi.org/project/yfinance/)
+    * [Requests](https://docs.python-requests.org/en/master/)
+
+
+## Description
+
+This program is run from the command line and accepts ETF symbols either as command arguments or from a text file in the local directory.
+The top 25 holdings of each ETF will be saved in the local directory as `<ETF_symbol>-holdings.csv`.
+
+An optional log file can be generated that contains a list of the successfully downloaded ETFs, their full names, and their previous closing price.
+
+## How to Use
+
+### Simple Example
+
+Generate a file in the local directory named VTI-holdings.csv containing top 25 holdings of the ETF:
+
+    $ python3 holdings_dl.py -s VTI
+
+###### Terminal Output
+```
+Retrieving holdings information
+VTI ... complete
+
+1 file(s) have been generated for 1 ETF(s):
+VTI-holdings.csv
+```
+
+###### VTI-holdings.csv
+
+|Symbol|Name          |% Weight|
+|:------:|--------------|--------|
+|MSFT  |Microsoft Corp|4.39%   |
+|AAPL  |Apple Inc     |4.39%   |
+| ... | ...           | ...    |
+
+
+### General Usage
+    holdings_dl.py [-h] (-f FILE | -s SYMBOL) [-q] [-l] [-a]
+
+      -f FILE, --file FILE  specify a file containing a list of ETF symbols
+      -s SYMBOL, --symbol SYMBOL
+                            specify an ETF symbol
+    optional arguments:
+      -h, --help            show help message and exit
+      -q, --quiet           suppress terminal output
+      -l, --log             create a log of the downloaded ETFs in etf-log.csv
+      -a, --alpha           sort the ETF symbols into alphabetical order for
+                            output
+
+
+### Input Modes
+
+There are two ways to provide input to the program: either by symbol or by file list. This is specified by the user as a required command line argument.
+
+####1. Use `--symbol` or `-s` to input an ETF symbol directly
+
+
+This was used in the simple example above, however it is worth noting that repeating the flag allows for the input of multiple symbols at a time.
+
+    $ python3 holdings_dl.py --symbol VTI -s SPY -s QQQ  
+
+
+####2. Use `--file` or `-f` to specify a text file
+
+    $ python3 holdings_dl.py -f MyFile.txt
+
+   A valid input file contains a plaintext list of ETF symbols, each followed by a newline. 
+   Only one file will be accepted at a time.
+
+Ex. `MyFile.txt`
+
+        VTI\n
+        SPY\n
+        QQQ\n
+
+
+### Generate Log
+Specify `--log` or `-l` at the command line in order to 
+generate a file named `etf-log.csv` in the local directory. 
+This file will contain a log of all the ETFs that were downloaded by the program. 
+The full name and previous closing price of each ETF are listed as well.
+
+Using the `--alpha` or `-a` flag will sort the ETFs alphabetically by symbol in the log file.
+
+#### Example:
+
+     $ python3 holdings_dl.py -s VTI -s SPY -s QQQ -l -a
+
+###### Terminal Output
+```
+Retrieving holdings information
+QQQ ... complete
+SPY ... complete
+VTI ... complete
+Generating etf-log.csv ... complete
+
+4 file(s) have been generated for 3 ETF(s):
+etf-log.csv
+QQQ-holdings.csv
+SPY-holdings.csv
+VTI-holdings.csv
+```
+
+###### etf-log.csv
+
+|Symbol|Name          |Previous Closing Price|
+|------|--------------|----------------------|
+|QQQ   |Invesco QQQ Trust, Series 1|358.77   |
+|SPY   |SPDR S&P 500  |430.92                |
+|VTI   |Vanguard Total Stock Market ETF|223.18 |
+
+## Resources
+All holdings data comes  from [YCharts.com](https://ycharts.com/stocks) via the pandas [read_html](https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_html.html) function.
+Information in `etf-log.csv` is retrieved using [yfinance](https://pypi.org/project/yfinance/).
+Command line arguments are processed with [argparse](https://docs.python-requests.org/en/master/).
+
+## Authors
+
+Piper Batey (pbatey@umich.edu)
+
+## Version History
+
+* 0.1
+    * Initial Release
+
+## License
+
+The MIT License (MIT)
+
+Copyright (c) 2021 Piper Batey
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ holdings_dl.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env
+import argparse
+import pandas as pd
+from urllib.error import HTTPError
+import yfinance as yf
+
+'''
+    File name: holdings_dl.py
+    Author: Piper Batey
+    Date created: 7/13/2021
+    Date last modified: 7/13/2021
+    Python Version: 3.8
+    Description: A simple Python script that downloads 
+    the top 25 holdings of one or more ETFs into a .csv file.
+'''
+
+
+class HoldingsDownloader:
+    def __init__(self):
+        # variables
+        self.etf_symbols = []
+        self.valid_etfs = []
+        self.file_name = ""
+        self.num_files = 0
+        self.log_mode = False
+        self.quiet_mode = False
+        self.sort_mode = False
+        # init
+        self._parse_command_args()
+        if self.file_name:
+            self._read_input_file()
+        if self.sort_mode:
+            self.etf_symbols.sort()
+
+    def _parse_command_args(self):
+        parser = argparse.ArgumentParser(description="creates a .csv file with the top 25 holdings of an ETF")
+        input_type_group = parser.add_mutually_exclusive_group(required=True)
+        input_type_group.add_argument("-f", "--file", help="specify a file containing a list of ETF symbols")
+        input_type_group.add_argument("-s", "--symbol", action='append', default=[],
+                                      help="specify an ETF symbol")
+        parser.add_argument("-q", "--quiet", action="store_true", help="suppress verbose terminal output")
+        parser.add_argument("-l", "--log", action="store_true",
+                            help="create a log of the downloaded ETFs in etf-log.csv")
+        parser.add_argument("-a", "--alpha", action="store_true",
+                            help="sort the ETF symbols into alphabetical order for output")
+        args = parser.parse_args()  # get args from the command line
+        if args.quiet:
+            self.quiet_mode = True
+        if args.log:
+            self.log_mode = True
+        if args.alpha:
+            self.sort_mode = True
+        if args.file:
+            self.file_name = args.file
+        if args.symbol:
+            self.etf_symbols = args.symbol
+
+    def _read_input_file(self):
+        if not self.quiet_mode:
+            print("Reading symbols from {} ...".format(self.file_name), end=" ")
+        with open(self.file_name, 'r') as input_file:
+            for name in input_file:
+                self.etf_symbols.append(name[:-1])  # avoid the newline at the end
+        if not self.quiet_mode:
+            print("complete")
+
+    # Reads ETFs.txt and returns a sorted list of the ETF names
+    def generate_csv_files(self):
+        if not self.quiet_mode:
+            print("Retrieving holdings information")
+        for etf_symbol in self.etf_symbols:
+            url = "https://ycharts.com/companies/{}/holdings".format(etf_symbol)
+            if not self.quiet_mode:
+                print("{} ...".format(etf_symbol), end=" ")
+            try:
+                dataframe_list = pd.read_html(url, match="Symbol")  # get list of dataframes from website
+                dataframe = dataframe_list[0]
+                dataframe.columns = ["Symbol", "Name", "% Weight", "Price", "% Change"]  # avoid whitespace issues
+                dataframe = dataframe.drop(columns=["% Change", "Price"])  # delete unnecessary columns
+                dataframe.to_csv("{}-holdings.csv".format(etf_symbol), index=False)  # create the csv
+                self.valid_etfs.append(etf_symbol)
+                self.num_files += 1
+                if not self.quiet_mode:
+                    print("complete")
+            except HTTPError:
+                if not self.quiet_mode:
+                    print("ERROR\n{} is not found as an ETF. Holdings file not generated.".format(etf_symbol))
+
+    def generate_etf_log_file(self):
+        etf_full_names = []
+        prev_close_prices = []
+        if not self.quiet_mode:
+            print("Generating etf-log.csv ...", end=" ")
+
+        # Download the full name and previous closing price of each successfully fetched ETF
+        for symbol in self.valid_etfs:
+            etf_ticker = yf.Ticker(symbol)
+            etf_full_names.append(etf_ticker.info["shortName"])
+            prev_close_prices.append(etf_ticker.info["previousClose"])
+
+        # use a pandas dataframe to create the csv
+        log_dataframe = pd.DataFrame({"Symbol": self.valid_etfs,
+                                      "Name": etf_full_names,
+                                      "Previous Closing Price": prev_close_prices})
+        log_dataframe.to_csv("etf-log.csv", index=False)
+        self.num_files += 1
+        if not self.quiet_mode:
+            print("complete")
+
+
+def main():
+    downloader = HoldingsDownloader()
+    downloader.generate_csv_files()
+    if downloader.log_mode:
+        downloader.generate_etf_log_file()
+
+    if not downloader.quiet_mode:  # verbose output
+        print("\n{} file(s) have been generated for {} ETF(s):".format(downloader.num_files,
+                                                                       len(downloader.valid_etfs)))
+        if downloader.log_mode:
+            print("etf-log.csv")
+        for symbol in downloader.valid_etfs:
+            print("{}-holdings.csv".format(symbol))
+
+
+if __name__ == "__main__":
+    main()
